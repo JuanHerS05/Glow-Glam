67068632a28bb684be13fa8cb6d622dc900361b2
Juan Luis Herrera Suárez <159280622+JuanHerS05@users.noreply.github.com>
2026-07-14 19:16:55 -0500

    Remove logout link and add admin home link

--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -186,12 +186,7 @@ export default function AddProduct() {
                 </li>
               </ul>
             </li>
-
-            <li className="menuItem">
-              <Link to="#" className="menuLink" onClick={handleLogout}>
-                <i className="fas fa-sign-out-alt"></i> Cerrar sesión
-              </Link>
-            </li>
+<li><Link to="/adminHome"><i className="fas fa-sign-out-alt"></i> Volver a Inicio</Link></li>
           </ul>
         </nav>
       </header>
